591edf4e8b7a5834a73ecfd24a3b4493ad987581
Stepan Roznik <stepan@roznik.cz>
2023-12-12 09:50:17 +0100

    feat: 10-1 solution

--- day-10/solution-1.ts
+++ day-10/solution-1.ts
@@ -1,5 +1,12 @@
 import input from './input.txt';
 
+// const input = `
+// 7-F7-
+// .FJ|7
+// SJLL7
+// |F--J
+// LJ.LJ`;
+
 type Direction = 'up' | 'down' | 'left' | 'right';
 
 enum Pipe {
@@ -47,16 +54,14 @@ const pipeConnectionsMap: Record<Pipe, Partial<Record<keyof typeof directions, P
 
 const pipeMap = input.split('\n');
 
-let previousPosition: [number, number] = [0, 0];
-let currentPosition = previousPosition;
 let currentType = 'S' as Pipe | 'S';
-let stepsTaken = 0;
+const positionsTaken = [] as [number, number][];
 
 // Determine S position
 for (const y in pipeMap) {
     const x = [...pipeMap[y]].findIndex((pipe) => pipe === 'S');
     if (x > -1) {
-        currentPosition = [x, +y];
+        positionsTaken.push([x, +y]);
         break;
     }
 }
@@ -65,6 +70,7 @@ for (const y in pipeMap) {
 for (const pipeKey in Pipe) {
     const possibleDirs: Direction[] = [];
     const pipe = Pipe[pipeKey as keyof typeof Pipe];
+    const currentPosition = positionsTaken[positionsTaken.length - 1];
     for (const dir in pipeConnectionsMap[pipe]) {
         const direction = dir as Direction;
         const [dx, dy] = directions[direction];
@@ -79,25 +85,30 @@ for (const pipeKey in Pipe) {
     }
 }
 
+function positionsMatch(position1: [number, number], position2: [number, number]): boolean {
+    return position1[0] === position2[0] && position1[1] === position2[1];
+}
+
 for (;;) {
+    let wasFound = false;
+    const currentPosition = positionsTaken[positionsTaken.length - 1];
+    const previousPosition = positionsTaken[positionsTaken.length - 2] || [0, 0];
     Object.entries(pipeConnectionsMap[currentType as Pipe]).forEach(([dir, possiblePipes]) => {
         const direction = dir as Direction;
         const [dx, dy] = directions[direction];
         const [newX, newY] = [currentPosition[0] + dx, currentPosition[1] + dy];
         const pipeAtDir = pipeMap[newY][newX] as Pipe | 'S';
         if (
-            !(previousPosition[0] === newX && previousPosition[1] === newY) &&
+            !positionsMatch(previousPosition, [newX, newY]) &&
+            !wasFound &&
             (pipeAtDir === 'S' || possiblePipes.includes(pipeAtDir))
         ) {
-            stepsTaken++;
+            wasFound = true;
             currentType = pipeAtDir;
-            previousPosition = currentPosition;
-            currentPosition = [newX, newY];
-            console.log(currentType, currentPosition);
-            return;
+            positionsTaken.push([newX, newY]);
         }
     });
     if (currentType === 'S') break;
 }
 
-console.log(stepsTaken);
+console.log((positionsTaken.length - 1) / 2);
