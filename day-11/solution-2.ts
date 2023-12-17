c428a68dbb055fdf5f9c862497fe14bf7213b1b4
Stepan Roznik <stepan@roznik.cz>
2023-12-18 00:18:59 +0100

    feat: improved readability (at least for me)

--- day-11/solution-2.ts
+++ day-11/solution-2.ts
@@ -1,7 +1,6 @@
 import input from './input.txt';
 
 const multiplier = 1000000;
-
 const image = input.split(/\n/);
 const rowLen = image[0].length;
 
@@ -18,18 +17,12 @@ image.forEach((row, y) => {
 
 const sum = galaxyPositions.reduce((total, [x, y], index) => {
     const sumOfDistancesFromOthers = galaxyPositions.slice(index + 1).reduce((totalOfOthers, [otherX, otherY]) => {
-        const emptyColsInBetween = emptyCols.filter(
-            (col) => (col < x && col > otherX) || (col < otherX && col > x)
-        ).length;
-        const emptyRowsInBetween = emptyRows.filter(
-            (row) => (row < y && row > otherY) || (row < otherY && row > y)
-        ).length;
-        const distanceSum =
-            Math.abs(x - otherX) +
-            emptyColsInBetween * (multiplier - 1) +
-            Math.abs(y - otherY) +
-            emptyRowsInBetween * (multiplier - 1);
-        return totalOfOthers + distanceSum;
+        const emptyColsInBetween = emptyCols.filter((c) => (c < x && c > otherX) || (c < otherX && c > x)).length;
+        const emptyRowsInBetween = emptyRows.filter((r) => (r < y && r > otherY) || (r < otherY && r > y)).length;
+
+        const distanceX = Math.abs(x - otherX) + emptyColsInBetween * (multiplier - 1);
+        const distanceY = Math.abs(y - otherY) + emptyRowsInBetween * (multiplier - 1);
+        return totalOfOthers + distanceX + distanceY;
     }, 0);
     return total + sumOfDistancesFromOthers;
 }, 0);
